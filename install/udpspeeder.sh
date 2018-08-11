c66484238353764ef5eb68934c2718dddaefaf8b
hxzmm <hxzmm@qq.com>
2018-08-11 23:55:45 +0800

    Update udpspeeder.sh

--- install/udpspeeder.sh
+++ install/udpspeeder.sh
@@ -1,11 +1,10 @@
 if [ -d "UDPspeeder" ]; then
     cd UDPspeeder
-    nohup ./speederv2 -s -l 0.0.0.0:20000 -r 127.0.0.1:8833  -f20:10 &
 else
     mkdir UDPspeeder
     cd UDPspeeder
     wget https://github.com/wangyu-/UDPspeeder/releases/download/20170806.0/speederv2_linux.tar.gz
     tar zxf speederv2_linux.tar.gz
     mv speederv2_amd64 speederv2
-    nohup ./speederv2 -s -l 0.0.0.0:20000 -r 127.0.0.1:8833  -f20:10 &
 fi
+nohup ./speederv2 -s -l 0.0.0.0:20000 -r 127.0.0.1:8833  -f20:10 &
