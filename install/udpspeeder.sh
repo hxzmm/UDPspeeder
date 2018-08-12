406ba5476bf64335e48730a0da463c2b5295362a
hxzmm <hxzmm@qq.com>
2018-08-13 05:16:18 +0800

    Update udpspeeder.sh

--- install/udpspeeder.sh
+++ install/udpspeeder.sh
@@ -1,10 +1,8 @@
-if [ -d "UDPspeeder" ]; then
-    cd UDPspeeder
-else
-    mkdir UDPspeeder
-    cd UDPspeeder
-    wget https://github.com/wangyu-/UDPspeeder/releases/download/20170806.0/speederv2_linux.tar.gz
-    tar zxf speederv2_linux.tar.gz
-    mv speederv2_amd64 speederv2
-fi
-nohup ./speederv2 -s -l 0.0.0.0:20000 -r 127.0.0.1:8833  -f20:10 &
+mkdir udpspeeder
+cd udpspeeder
+wget https://github.com/wangyu-/UDPspeeder/releases/download/20170806.0/speederv2_linux.tar.gz
+tar zxf speederv2_linux.tar.gz
+mv speederv2_amd64 speederv2
+wget https://raw.githubusercontent.com/hxzmm/UDPspeeder/branch_libev/install/udpspeeder.conf
+wget -O /etc/init.d/udpspeeder https://raw.githubusercontent.com/hxzmm/UDPspeeder/branch_libev/install/udpspeeder
+chmod 755 /etc/init.d/udpspeeder; update-rc.d udpspeeder defaults; service udpspeeder start
