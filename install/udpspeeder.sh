970644e9cdc27146a2cc1cea296c0cca77744331
hxzmm <hxzmm@qq.com>
2026-03-01 15:13:47 +0800

    Update udpspeeder installation to use systemd
    Replace init.d script installation with systemd service setup for udpspeeder.

--- install/udpspeeder.sh
+++ install/udpspeeder.sh
@@ -4,5 +4,9 @@ wget https://github.com/wangyu-/UDPspeeder/releases/download/20180806.0/speederv
 tar zxf speederv2_linux.tar.gz
 mv speederv2_amd64 speederv2
 wget https://raw.githubusercontent.com/hxzmm/UDPspeeder/master/install/udpspeeder.conf
-wget -O /etc/init.d/udpspeeder https://raw.githubusercontent.com/hxzmm/UDPspeeder/master/install/udpspeeder
-chmod 755 /etc/init.d/udpspeeder; update-rc.d udpspeeder defaults; service udpspeeder start
+wget -O /etc/systemd/system/udpspeeder.service https://raw.githubusercontent.com/hxzmm/UDPspeeder/master/install/udpspeeder
+chmod 644 /etc/systemd/system/udpspeeder.service
+systemctl daemon-reload
+systemctl enable udpspeeder
+systemctl restart udpspeeder
+systemctl status udpspeeder
